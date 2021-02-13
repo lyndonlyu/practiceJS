e6a82c10e5494fdd3419975a512990fc00aeeda4
lyndon lyu <7yeahslv@gmail.com>
2021-02-14 00:30:49 +0100

    prepare for code challenge

--- javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -47,7 +47,7 @@ console.log(typeof year);
 //important bug
 //it returns object
 console.log(typeof null);
-*/
+
 
 //let and const was introduce in ES6 
 //the legacy way of declaring variable is using var, prior to ES6
@@ -67,3 +67,54 @@ const birthYear = 1993;
 //this would work but!! it does not create variable in the scope, instead Javascript will create a property on the global object.
 lastName = "susan";
 console.log(lastName);
+
+
+
+// Math operators
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas,ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+
+const firstName = 'Lyndon';
+const lastName = 'Lyu';
+console.log(firstName + ' ' + lastName);
+
+
+// Assignment operators
+let x = 10 + 5; // 15
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1
+x--;
+x--;
+console.log(x);
+
+// Comparison operators
+console.log(ageJonas > ageSarah); // >, <, >=, <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
+
+////////////////////////////////////
+// Operator Precedence
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5; // x = y = 10, x = 10
+console.log(x, y);
+
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah, averageAge);
+
+*/
