63317d7248cf962e12711146fffbe98ec08c8620
lyndon lyu <7yeahslv@gmail.com>
2021-02-13 22:46:29 +0100

    Add name conventions and variables

--- javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,23 @@
-let js = 'amazing';
-if (js === 'amazing')
-  alert('JavaScript is fun')
-console.log(40 + 8 + 10);
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Matilda";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// Variable name conventions
+let jonas_matilda = "JM";
+let $function = 27;
+
+let person = "jonas";
+let PI = 3.1415;
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
+
+let job1 = "programmer";
+let job2 = "teacher";
+
+console.log(myFirstJob);
