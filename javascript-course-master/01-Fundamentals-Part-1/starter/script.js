0e27682789c0dc7eda17a4065299d5b17cb68f09
lyndon lyu <7yeahslv@gmail.com>
2021-02-13 23:15:41 +0100

    dynamic typing and the legacy bug of null

--- javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,4 @@
+/*
 console.log("Jonas");
 console.log(23);
 
@@ -21,3 +22,28 @@ let job1 = "programmer";
 let job2 = "teacher";
 
 console.log(myFirstJob);
+*/
+
+//let javaScript = true;
+//console.log(javaScript);
+
+//the typeof is operator like +,- and the result of typeof operator is a string witht he type of the value.
+
+//console.log(typeof true);
+console.log(typeof javaScript);
+//console.log(typeof 23);
+//console.log(typeof "String");
+
+//dynamic typing
+javaScript = "Yay!";
+console.log(typeof javaScript);
+
+
+//no value defined, then the type is undefined
+let year;
+console.log(year);
+console.log(typeof year);
+
+//important bug
+//it returns object
+console.log(typeof null);
