9e52990347a5c4bd3a2e45959cc702cd6389e5cc
lyndon lyu <7yeahslv@gmail.com>
2021-02-14 01:28:39 +0100

    finish code challenge

--- javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -118,3 +118,17 @@ const averageAge = (ageJonas + ageSarah) / 2;
 console.log(ageJonas, ageSarah, averageAge);
 
 */
+
+
+let massMark = 78;
+let heightMark = 1.69;
+let massJohn= 92;
+let heightJohn = 1.95;
+
+let BMIJohn = massJohn/heightJohn **2;
+let BMIMark = massMark/heightMark **2;
+
+let markHigherBMI = BMIMark > BMIJohn;
+
+console.log(BMIJohn,BMIMark);
+console.log(markHigherBMI);
